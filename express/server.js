06e0fa86c40cf1c3403bd1ce7e17587d11030fd3
qcjn <40976058+qcjn@users.noreply.github.com>
2019-10-24 06:22:14 +0800

    Update server.js

--- express/server.js
+++ express/server.js
@@ -12,6 +12,7 @@ router.get('/', (req, res) => {
   res.end();
 });
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
+router.get('/*', (req, res) => res.text('OK'));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
 
 app.use(bodyParser.json());
